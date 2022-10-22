d4f48ccf9e915d0f540f433d03fee9896ed5a48a
ChandanC <chandan@viba.ai>
2022-10-22 11:05:59 +0530

    Refactoring in extension

--- Sources/NetworkClient/Extensions/URL+Extensions.swift
+++ Sources/NetworkClient/Extensions/URL+Extensions.swift
@@ -2,6 +2,9 @@ import UIKit
 
 extension URL {
     var isValid: Bool {
-        return ((self.host != nil) && (self.scheme != nil) && UIApplication.shared.canOpenURL(self))
+        return (
+            (self.host != nil)
+            && (self.scheme != nil)
+            && UIApplication.shared.canOpenURL(self))
     }
 }
